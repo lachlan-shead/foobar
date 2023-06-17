e311632e60fb45aada5a128f6c20379656eb6b1c
lshe2027 <lshe2027@uni.sydney.edu.au>
2023-06-17 15:17:54 +1000

    Create initial e2e tests

---
+++ task-4/src/TestGivenCases.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class TestGivenCases {
+
+    static BiConsumer<int[][], int[]> testLambda = (in, exp) -> {
+        int[] out = Solution.solution(in);
+        List<Integer> outList = IntStream.of(out).boxed().collect(Collectors.toList());
+        List<Integer> expList = IntStream.of(exp).boxed().collect(Collectors.toList());
+        assertEquals(expList, outList,
+            "Real and expected mismatch");
+    };
+
+    @Test
+    public void test1() {
+        testLambda.accept(new int[][]{
+            {0, 2, 1, 0, 0},
+            {0, 0, 0, 3, 4},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        }, new int[]{7, 6, 8, 21});
+    }
+
+    @Test
+    public void test2() {
+        testLambda.accept(new int[][] {
+            {0, 1, 0, 0, 0, 1},
+            {4, 0, 0, 3, 2, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
+        }, new int[]{0, 3, 2, 9, 14});
+    }
+}
---
+++ task-4/src/TestSimpleCases.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class TestSimpleCases {
+
+    static BiConsumer<int[][], int[]> testLambda = (in, exp) -> {
+        int[] out = Solution.solution(in);
+        List<Integer> outList = IntStream.of(out).boxed().collect(Collectors.toList());
+        List<Integer> expList = IntStream.of(exp).boxed().collect(Collectors.toList());
+        assertEquals(expList, outList,
+            "Real and expected mismatch");
+    };
+
+    @Test
+    public void testCaseWithOneState() {
+        testLambda.accept(new int[][]{{0}}, new int[]{1, 1});
+    }
+
+    @Test
+    public void testCasesWithTwoStates() {
+        testLambda.accept(new int[][]{{0, 0}, {0, 0}}, new int[]{1, 0, 1});
+        testLambda.accept(new int[][]{{0, 0}, {2, 5}}, new int[]{1, 1});
+        testLambda.accept(new int[][]{{0, 1}, {0, 0}}, new int[]{1, 1});
+        testLambda.accept(new int[][]{{5, 1}, {0, 0}}, new int[]{1, 1});
+    }
+}
